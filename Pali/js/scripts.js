aea889106f701b281c755a92c65e545a98cdb8c4
maicolkutchoukov <maicol.j.95@gmail.com>
2023-12-12 13:40:13 +0100

    Creata funzione parola palindroma

--- Pali/js/scripts.js
+++ Pali/js/scripts.js
@@ -1,1 +1,23 @@
-console.log('Template html-css-js');
+/*
+    Chiedere all'utente di inserire una parola
+    Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+
+// .split("")               crea un'array dei caratteri contenuti nella parola
+// .reverse()               inverte l'ordine dell'array
+//.join("")                 restituisce un array sotto forma di stringa
+
+function isPalindrom(word) {
+    const wordReverse = word.split("").reverse().join("");
+    if (word === wordReverse){
+        return true
+    }
+    return false
+}
+  
+const myWord = prompt('Inserisci una parola');
+
+const isPalindroma = isPalindrom(myWord);
+  
+console.log("La parola è palindroma?", isPalindroma);
