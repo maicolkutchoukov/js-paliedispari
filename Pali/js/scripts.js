45656ed186e3a4153ae3c2ab9e33cd158e11fdb9
maicolkutchoukov <maicol.j.95@gmail.com>
2023-12-12 14:08:47 +0100

    Aggiunta parte grafica

--- Pali/js/scripts.js
+++ Pali/js/scripts.js
@@ -9,15 +9,29 @@
 //.join("")                 restituisce un array sotto forma di stringa
 
 function isPalindrom(word) {
+    
     const wordReverse = word.split("").reverse().join("");
+    
     if (word === wordReverse){
         return true
     }
     return false
 }
   
-const myWord = prompt('Inserisci una parola');
+const myWord = document.getElementById('word').value;
 
-const isPalindroma = isPalindrom(myWord);
-  
-console.log("La parola è palindroma?", isPalindroma);
+const myButton = document.querySelector('button');
+myButton.addEventListener('click', function(){
+    const myWord = document.getElementById('word').value.toLowerCase();
+    console.log(myWord);
+    const isPalindroma = isPalindrom(myWord);
+    console.log("La parola è palindroma?", isPalindroma);
+    let risultato = document.querySelector('p');
+    if (isPalindroma){
+        risultato.innerHTML = 'La parola inserita è un palindromo';
+    }
+    else {
+        risultato.innerHTML = 'La parola inserita non è un palindromo';
+
+    }
+})
